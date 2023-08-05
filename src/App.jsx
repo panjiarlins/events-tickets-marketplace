c2daf18b822ce3b2e1ff61a4a01f90c712a60333
wfhadit <wfhadit@gmail.com>
2023-08-05 17:28:55 +0700

    dasboard t

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import { Route, Routes } from "react-router-dom";
+import Dashboard from './dashboard/dashboard';
 
 const App = () => {
-  return <div></div>;
+  return (
+  <>
+  <Routes>
+    <Route path="dashboard" element={<Dashboard/>}/>
+
+
+  </Routes>
+  </>
+  );
 };
 
 export default App;
