391c00077c55063866303ea899f8840e710e5693
Moch Ridwan Qodri <ridwanqodryy@gmail.com>
2023-08-05 15:06:05 +0700

    create navbar, login, register, details

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
-import React from 'react';
+import React from "react";
+import { Login } from "./pages/login";
+import { Register } from "./pages/register";
+import { Route, Routes } from "react-router-dom";
+import { Navbar, toggleMenu } from "./pages/navbar";
+import { DetailsProduct } from "./pages/details";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/details" element={<DetailsProduct />} />
+      </Routes>
+    </>
+  );
 };
 
 export default App;
