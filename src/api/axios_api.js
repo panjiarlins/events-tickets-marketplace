78566484d417e96a3c869218101f0f9861c355ab
Panji Arlin Saputra <75957797+panjiarlins@users.noreply.github.com>
2023-08-11 09:06:09 +0700

    Update axios_api.js

--- src/api/axios_api.js
+++ src/api/axios_api.js
@@ -1,8 +1,13 @@
 import axios from 'axios';
 
-const LOCAL_HOST = 'http://192.168.102.216';
+const protocol = window.location.protocol;
+const hostname = window.location.hostname;
 
-const web_host = `http://${LOCAL_HOST}:3000`;
+const web_host = `${protocol}//${hostname}`;
+
+// const LOCAL_HOST = 'http://192.168.102.216';
+
+// const web_host = `http://${LOCAL_HOST}:3000`;
 
 const axios_api = axios.create({
   baseURL: `https://api-mini-project.glitch.me`,
